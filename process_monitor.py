e4921b84e1e5e273bb54e50f39d3fd702d4c4c45
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2018-02-21 10:32:08 -0700

    basic process log

---
+++ process_monitor.py
@@ -0,0 +1,45 @@
+
+import win32con
+import win32api
+import win32security
+
+import wmi
+import sys
+import os
+
+def log_to_file(message):
+    fd = open("process_monitor_log.csv", "ab")
+    fd.write("%s\r\n" % message)
+    fd.close()
+
+    return
+
+log_to_file("Time,User,Executable,CommandLine,PID,Parent PID,Privilieges")
+
+c = wmi.WMI()
+
+process_watcher = c.Win32_Process.watch_for("creation")
+
+while True:
+    try:
+        new_process = process_watcher()
+
+        proc_owner  = new_process.GetOwner()
+        proc_owner  = "%s\\%s" & (proc_owner[0],proc_owner[2])
+        create_date = new_process.CreationDate
+        executable  = new_process.ExecutablePath
+        cmdline     = new_process.CommandLine
+        pid         = new_process.ProcessId
+        parent_pid  = new_process.ParentProcessId
+
+        priviliages = "N/A"
+
+        process_log_messages  = "%s, %s, %s, %s, %s, %s, %s\r\n" % (create_date,
+                proc_owner, excecutable, cmdline, pid, parent_pid, privileges)
+
+        print process_log_message
+
+        log_to_file(process_log_message)
+
+    except:
+        pass
